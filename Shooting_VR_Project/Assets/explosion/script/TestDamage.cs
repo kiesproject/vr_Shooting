1a9cae99512f6aaed764941ac65061c929f70a19
宮澤　瑛至 <b1800722@planet.kanazawa-it.ac.jp>
2019-03-01 21:53:56 +0900

    爆発のエフェクトと爆発の当たり判定を実装しました

---
+++ Shooting_VR_Project/Assets/explosion/script/TestDamage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDamage : MonoBehaviour
+{
+    [SerializeField] int HP = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Hit(int damage)
+    {
+        Debug.Log("Hit!!");
+
+        HP -= damage;
+        Debug.Log(HP);
+
+        if (HP <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Shooting_VR_Project/Assets/explosion/script/TestLuncher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLuncher : MonoBehaviour
+{
+    public GameObject Bomb;
+    public float shotSpeed = 20.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shot();
+        }
+    }
+
+    void Shot()
+    {
+        GameObject @object = (GameObject)Instantiate(
+                Bomb,
+                this.transform.position,
+                Quaternion.identity
+            );
+
+        Rigidbody objectRigitbody = @object.GetComponent<Rigidbody>();
+        objectRigitbody.AddForce(this.transform.forward * shotSpeed);
+    }
+}
