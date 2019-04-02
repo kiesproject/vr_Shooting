163e8d430f41866fec74c03b72f9136db5a65532
宮澤　瑛至 <b1800722@planet.kanazawa-it.ac.jp>
2019-04-02 19:19:40 +0900

    中ボスの挙動を実装

---
+++ Shooting_VR_Project/Assets/middleBoss/script/BarrierGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class BarrierGenerator : MonoBehaviour
+{
+    public GameObject[] Beacons;
+
+    public PlayableDirector playableDirector;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Beacons = GameObject.FindGameObjectsWithTag("Beacon");
+
+        if (Beacons.Length <= 0)
+        {
+            Debug.Log("break!!");
+            playableDirector.Play();
+        }
+    }
+}
---
+++ Shooting_VR_Project/Assets/middleBoss/script/ExplosionController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour
+{
+    public GameObject Effect;
+
+    public float Range = 10.0f;
+    public float Force = 100.0f;
+
+    [SerializeField] int Damage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 hitPosition = collision.contacts[0].point;
+        Instantiate(Effect, hitPosition, Quaternion.identity);
+
+        Collider[] colliders = Physics.OverlapSphere(hitPosition, Range);
+        foreach(Collider obj in colliders)
+        {
+            if(obj.gameObject.tag == "Beacon")
+            {
+                obj.GetComponent<Rigidbody>().AddExplosionForce(Force, hitPosition, Range);
+                obj.GetComponent<Beacon>().Damage(Damage);
+            }
+        }
+
+        Destroy(this.gameObject);
+    }
+
+}
