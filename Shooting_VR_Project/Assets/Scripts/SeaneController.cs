1adb9d0927ab41400cf58ac4c088fb01aa8b044a
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2019-06-06 22:04:37 +0900

    テスト用のレベルシーンを作成

---
+++ Shooting_VR_Project/Assets/Scripts/SeaneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SeaneController : MonoBehaviour
+{
+
+    public static string[] _sceneSequence;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        Debug.Log("ActiveScene : " + scene.name);
+    }
+}
