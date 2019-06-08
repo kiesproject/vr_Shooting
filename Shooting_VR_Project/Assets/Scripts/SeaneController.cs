43355973fca6d27c8b87b83d032e00b514582665
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2019-06-08 20:53:18 +0900

    シーンを切り替えるSceanControllerを追加

--- Shooting_VR_Project/Assets/Scripts/SeaneController.cs
+++ Shooting_VR_Project/Assets/Scripts/SeaneController.cs
@@ -5,13 +5,43 @@ using UnityEngine.SceneManagement;
 
 public class SeaneController : MonoBehaviour
 {
+    public static SeaneController sceanController;
+    static int nowSceanIndex = 0;
 
-    public static string[] _sceneSequence;
-    
+    [SerializeField] string[] _sceneSequence;
+
+    //デストロイできないオブジェクトに指定
+    private void Awake()
+    {
+        if (sceanController == null) sceanController = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Scene scene = SceneManager.GetActiveScene();
-        Debug.Log("ActiveScene : " + scene.name);
+        //先頭のシーン以外アンロード(1からスタート)
+        for (int i = 1; i < _sceneSequence.Length; i++)
+        {
+            SceneManager.UnloadScene(_sceneSequence[i]);
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log("nowScean " + nowSceanIndex);
+        //テスト用にNキーでシーン切り替え
+        if (Input.GetKeyDown(KeyCode.N)) SwitchScean();
+    }
+
+    //シーン切り替えメソッド
+    public void SwitchScean()
+    {
+        if(nowSceanIndex != _sceneSequence.Length - 1)
+        {
+            SceneManager.LoadSceneAsync(_sceneSequence[nowSceanIndex + 1],LoadSceneMode.Additive);
+            SceneManager.UnloadScene(_sceneSequence[nowSceanIndex]);
+            nowSceanIndex++;
+        }
     }
 }
